0f05ce658831c439523caae75a453ac6bc2e2c32
lanasamohina <lana.samohina@gmail.com>
2018-03-04 23:01:10 +0200

    first commit

---
+++ alert.js
@@ -0,0 +1,85 @@
+
+//функция прощания
+function goodbye() {
+    alert('До новых встреч!');
+}
+
+//функция расчета процента
+function calculateRate(deposit, term) {
+    switch (term) {
+        case 2:
+            if (deposit < 50000) {
+                return 3;
+            } else if (deposit < 100000) {
+                return 5;
+            } else {
+                return 10;
+            }
+        case 6:
+            if (deposit < 50000) {
+                return 5;
+            } else if (deposit < 100000) {
+                return 10;
+            } else {
+                return 15;
+            }
+        case 12:
+            if (deposit < 50000) {
+                return 10;
+            } else if (deposit < 100000) {
+                return 15;
+            } else {
+                return 20;
+            }
+        default:
+            return 3;
+    }
+}
+
+//основная функция
+function deposit() {
+
+    let deposit = prompt('Введите сумму депозита: от 5000 до 200000');
+
+    if (!isNaN(deposit) && deposit >= 5000 && deposit <= 200000) {
+        if(confirm('Вы уверены что хотите вложить сумму в размере ' + deposit + '?')){
+            let term = parseInt(prompt('Выберите срок депозита: 2, 6 или 12 мес'));
+            if (term === 2 || term === 6 || term === 12) {
+                let rate = calculateRate(deposit, term);
+                let month_ending = (term == 2) ? 'a' : 'ев';
+                alert(`Вы хотите вложить ${deposit} на срок ${term} месяц${month_ending}. \nВаша процентная ставка ${rate}%`);
+                goodbye();
+            } else if(term == null){
+                goodbye();
+            } else {                alert('Вы указали неверный срок депозита')
+            }
+        } else {
+            goodbye();
+        }
+    } else if (deposit == null){
+        goodbye();
+    } else {
+        alert('Введите корректную сумму');
+    }
+}
+
+function pow_analog() {
+    let number = prompt('Введите число', '');
+    if (!isNaN(number)) {
+        let exponentiation = prompt('В какую степень возвести?');
+        if (!isNaN(exponentiation)) {
+            alert('Результат :' + toExponent(number,exponentiation));
+        } else
+            alert('Вы ввели не число!');
+    } else
+        alert('Вы ввели не число!');
+
+}
+
+function toExponent(number, exponentiation) {
+    let result = number;
+    for (i=1; i<exponentiation; i++) {
+        result = result * number;
+    }
+    return result;
+}
---
+++ js/main.js
@@ -0,0 +1,141 @@
+//var n = 3.14;
+
+//browser API alert (modal window)
+//alert(n)
+
+/*{
+    let count = 5;
+
+    let firstName = 'lana',
+        lastName = 'samohina',
+        age = null;
+
+}
+
+let str = '';
+
+for (let i = 0; i < 5000; i++) {
+    str = i
+}
+
+let firstName = 'lana',
+    lastName = 'samohina';
+let fullName = firstName + lastName;
+/!*console.log(fullName);*!/
+
+let n = 5;
+let suffix = 'px';
+let width = n + suffix;*/
+/*console.log(width); // any type + string = string*/
+
+//object
+
+/*let link = {
+    descr: 'Google',
+    path: 'https://google.com',
+    hoverTitle: 'Go to the search',
+    show: function () {
+        console.log('Awesome link', link.path)
+    },
+};
+
+link.show();*/
+/*
+
+console.log(link);
+console.log(link.descr);*/
+
+/*console.log(link.path + ' url');
+console.log(link.descr + 'Description');
+console.log(link['hover title'] + 'Hover title');*/
+
+/*let n = 1 + 2; //+-*!/%
+let n = 5 % 2; //остаток от деления 1*/
+/*
+let q = +prompt('Сколько тебе лет и я прибавлю еще 15. пы сы Паспорт', '');
+
+//console.log(Number(q) + 15);
+console.log(q + 15); //унарный плюс преобразовывает строку в число, если это возможно*/
+
+/*
+
+
+let n = 5;
+n += 5; //n = n + 5
+
+    */
+
+
+//let i = 0;
+//console.log('Старт цикла')
+
+/*while(i < 10) {
+    console.log(i);
+    i++;
+}*/
+/*=============================*/
+/*
+do {
+    console.log(i);
+    i++;
+} while (i < 1);
+
+===============================
+*/
+
+/*for (let i = 0; i < 10; i++) {
+    console.log(i)
+}
+
+=================================*/
+
+/*for (let i = 0; i<=10; i++){
+    if (i === 9){
+        //break;
+        continue;
+    }
+
+    console.log(i);
+}
+================================*/
+/*
+let min = +prompt('Ведите старт диапазона');
+let max = +prompt('Ведите конец диапазона');
+
+for (let i = min; i < max; 1++) {
+    if (i % 2 ==0) {
+        console.log('четное');
+    } else {
+        console.log('не четное');
+    }
+}
+==============================*/
+
+//console.log('Цикл завершен')
+
+/*
+1 % 3 === 0 to foo
+1 % 5 === 0 to bazz
+1 % 3 === 0 && i % 5 ===0 to fooBazz
+*/
+
+/*
+const db = 'Яблоко, Груша, Апельсин, Мандарин';
+let target = prompt('Какой продукт Вам интересен');
+
+if(target !== null){
+
+    let result = db.toLocaleLowerCase().indexOf(target.toLocaleLowerCase()); //-1 0, n
+    let answer = target[0].toLocaleUpperCase() + target.slice(1).toLocaleLowerCase();
+    if (result !== -1) {
+        console.log(`${answer} у нас есть. Покупайте.`);
+    } else {
+        console.log(`${answer} будет завтра.`);
+    }
+    console.log('Goodbay');
+}
+
+
+
+
+*/
